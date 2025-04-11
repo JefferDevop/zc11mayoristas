32b22de3fe0202358caed003e6132fbbda3c7e3a
JefferDevop <somadesarrollo@gmail.com>
2025-04-11 17:41:12 -0500

    My first commit

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,9 +3,9 @@ export const BASE_API = "https://zp.soluciones.today"
 
 
 export const SEARCH_HISTORY = "search-history";
-export const CART = "cart_zp";
-export const CARTPRODUCTS = "cart_produc_zp";
-export const TOKEN = "token_zp";
+export const CART = "cart_zp-mayoristas";
+export const CARTPRODUCTS = "cart_produc_zp-mayoristas";
+export const TOKEN = "token_zp-mayoristas";
 export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
 
 
