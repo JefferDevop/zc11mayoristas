02d9687fd15a38d44e3a8968967cc31324bad252
JefferDevop <somadesarrollo@gmail.com>
2025-04-13 10:28:03 -0500

    commit production

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,9 @@
-
 export const BASE_API = "https://zp.soluciones.today" 
 // export const BASE_API = "http://desarrollo.localhost:8000" 
 
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart_zp-mayoristas";
-export const CARTPRODUCTS = "cart_produc_zp-mayoristas";
+export const CARTPRODUCTS = "cart_product_zp-mayoristas";
 export const TOKEN = "token_zp-mayoristas";
 export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
-
-
